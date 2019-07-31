a2f70b66e0a2861845478eaf56a567cef8541996
Victor Jian Choe <vchoe89@gmail.com>
2019-07-31 19:20:35 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,4 +1,22 @@
-def guessing
+def run_guessing_game
+  puts "Guess a number between 1 and 6./n"
+  number = rand(1..6).to_s
+  
+  input = gets.chomp
+  
+  while input != "exit" do
+    if input == number
+      puts "You guessed the correct number"
+    else
+      puts "The computer guessed #{number}."
+    end
+    
+    puts "Guess a number between 1 and 6./n"
+    number = rand(1..6).to_s
+    input = gets.chomp
+  end
+  
+  puts "Goodbye!"
 
 end
 # def run_guessing_game
